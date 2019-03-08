16b74a26f5fa0fbb8502e397608480767f387d4e
John Kennedy Landazuri Sandoval <1060880355@academico.icesi.edu.co>
2019-03-08 10:11:47 -0500

    Seguimiento a completar

---
+++ S7_LANDAZURI_JOHN/src/ui/MoonController.java
@@ -0,0 +1,27 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.shape.Circle;
+
+public class MoonController {
+
+    @FXML
+    private Circle sunNoMove;
+
+    @FXML
+    private Circle moonToMove;
+    
+    
+    public void move() {
+    	
+    }
+
+    @FXML
+    void moveMoon(ActionEvent event) {   	
+    	
+
+    }
+
+}
+
